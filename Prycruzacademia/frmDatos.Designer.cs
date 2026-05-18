08c29d625d47a0e70ca80d74dce8b0ee60a7b33b
Alumno <Alumno@PC139>
2026-05-18 08:47:56 -0300

    Agregar archivos de proyecto.

---
+++ Prycruzacademia/frmDatos.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Prycruzacademia
+{
+    partial class frmDatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblPlan = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cmbPlan = new System.Windows.Forms.ComboBox();
+            this.lblActivo = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnListado = new System.Windows.Forms.Button();
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.Location = new System.Drawing.Point(43, 39);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(61, 16);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Codigo:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(43, 75);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(62, 15);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblPlan
+            // 
+            this.lblPlan.AutoSize = true;
+            this.lblPlan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlan.Location = new System.Drawing.Point(43, 112);
+            this.lblPlan.Name = "lblPlan";
+            this.lblPlan.Size = new System.Drawing.Size(42, 16);
+            this.lblPlan.TabIndex = 2;
+            this.lblPlan.Text = "Plan:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(111, 39);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(102, 20);
+            this.txtCodigo.TabIndex = 3;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(112, 75);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(101, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // cmbPlan
+            // 
+            this.cmbPlan.FormattingEnabled = true;
+            this.cmbPlan.Location = new System.Drawing.Point(92, 112);
+            this.cmbPlan.Name = "cmbPlan";
+            this.cmbPlan.Size = new System.Drawing.Size(121, 21);
+            this.cmbPlan.TabIndex = 5;
+            // 
+            // lblActivo
+            // 
+            this.lblActivo.AutoSize = true;
+            this.lblActivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblActivo.Location = new System.Drawing.Point(43, 156);
+            this.lblActivo.Name = "lblActivo";
+            this.lblActivo.Size = new System.Drawing.Size(50, 16);
+            this.lblActivo.TabIndex = 6;
+            this.lblActivo.Text = "Activo";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(99, 158);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnListado
+            // 
+            this.btnListado.BackColor = System.Drawing.Color.Indigo;
+            this.btnListado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnListado.Location = new System.Drawing.Point(18, 207);
+            this.btnListado.Name = "btnListado";
+            this.btnListado.Size = new System.Drawing.Size(75, 30);
+            this.btnListado.TabIndex = 8;
+            this.btnListado.Text = "Listado";
+            this.btnListado.UseVisualStyleBackColor = false;
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.BackColor = System.Drawing.Color.Purple;
+            this.btncancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncancelar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btncancelar.Location = new System.Drawing.Point(124, 207);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(75, 30);
+            this.btncancelar.TabIndex = 9;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.Pink;
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnRegistrar.Location = new System.Drawing.Point(221, 207);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 30);
+            this.btnRegistrar.TabIndex = 10;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            // 
+            // frmDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(317, 259);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btnListado);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblActivo);
+            this.Controls.Add(this.cmbPlan);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblPlan);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblCodigo);
+            this.Name = "frmDatos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro..";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblPlan;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ComboBox cmbPlan;
+        private System.Windows.Forms.Label lblActivo;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnListado;
+        private System.Windows.Forms.Button btncancelar;
+        private System.Windows.Forms.Button btnRegistrar;
+    }
+}
---
+++ Prycruzacademia/frmListado.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Prycruzacademia
+{
+    partial class frmListado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpListado = new System.Windows.Forms.GroupBox();
+            this.rdbTodo = new System.Windows.Forms.RadioButton();
+            this.rdbCodigo = new System.Windows.Forms.RadioButton();
+            this.rdbNombre = new System.Windows.Forms.RadioButton();
+            this.rdbPlan = new System.Windows.Forms.RadioButton();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cmbPlan = new System.Windows.Forms.ComboBox();
+            this.grpListado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpListado
+            // 
+            this.grpListado.Controls.Add(this.cmbPlan);
+            this.grpListado.Controls.Add(this.txtName);
+            this.grpListado.Controls.Add(this.txtCode);
+            this.grpListado.Controls.Add(this.rdbPlan);
+            this.grpListado.Controls.Add(this.rdbNombre);
+            this.grpListado.Controls.Add(this.rdbCodigo);
+            this.grpListado.Controls.Add(this.rdbTodo);
+            this.grpListado.Location = new System.Drawing.Point(27, 26);
+            this.grpListado.Name = "grpListado";
+            this.grpListado.Size = new System.Drawing.Size(322, 259);
+            this.grpListado.TabIndex = 0;
+            this.grpListado.TabStop = false;
+            this.grpListado.Text = "Buscar";
+            // 
+            // rdbTodo
+            // 
+            this.rdbTodo.AutoSize = true;
+            this.rdbTodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbTodo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.rdbTodo.Location = new System.Drawing.Point(39, 44);
+            this.rdbTodo.Name = "rdbTodo";
+            this.rdbTodo.Size = new System.Drawing.Size(57, 19);
+            this.rdbTodo.TabIndex = 0;
+            this.rdbTodo.TabStop = true;
+            this.rdbTodo.Text = "Todo";
+            this.rdbTodo.UseVisualStyleBackColor = true;
+            // 
+            // rdbCodigo
+            // 
+            this.rdbCodigo.AutoSize = true;
+            this.rdbCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbCodigo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.rdbCodigo.Location = new System.Drawing.Point(39, 81);
+            this.rdbCodigo.Name = "rdbCodigo";
+            this.rdbCodigo.Size = new System.Drawing.Size(70, 19);
+            this.rdbCodigo.TabIndex = 1;
+            this.rdbCodigo.TabStop = true;
+            this.rdbCodigo.Text = "Código";
+            this.rdbCodigo.UseVisualStyleBackColor = true;
+            // 
+            // rdbNombre
+            // 
+            this.rdbNombre.AutoSize = true;
+            this.rdbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbNombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.rdbNombre.Location = new System.Drawing.Point(39, 121);
+            this.rdbNombre.Name = "rdbNombre";
+            this.rdbNombre.Size = new System.Drawing.Size(76, 19);
+            this.rdbNombre.TabIndex = 2;
+            this.rdbNombre.TabStop = true;
+            this.rdbNombre.Text = "Nombre";
+            this.rdbNombre.UseVisualStyleBackColor = true;
+            // 
+            // rdbPlan
+            // 
+            this.rdbPlan.AutoSize = true;
+            this.rdbPlan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbPlan.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.rdbPlan.Location = new System.Drawing.Point(39, 156);
+            this.rdbPlan.Name = "rdbPlan";
+            this.rdbPlan.Size = new System.Drawing.Size(54, 19);
+            this.rdbPlan.TabIndex = 3;
+            this.rdbPlan.TabStop = true;
+            this.rdbPlan.Text = "Plan";
+            this.rdbPlan.UseVisualStyleBackColor = true;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(115, 80);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(72, 20);
+            this.txtCode.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(115, 120);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(87, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // cmbPlan
+            // 
+            this.cmbPlan.FormattingEnabled = true;
+            this.cmbPlan.Location = new System.Drawing.Point(115, 154);
+            this.cmbPlan.Name = "cmbPlan";
+            this.cmbPlan.Size = new System.Drawing.Size(121, 21);
+            this.cmbPlan.TabIndex = 6;
+            // 
+            // frmListado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(375, 302);
+            this.Controls.Add(this.grpListado);
+            this.ForeColor = System.Drawing.Color.MistyRose;
+            this.Name = "frmListado";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Listado..";
+            this.grpListado.ResumeLayout(false);
+            this.grpListado.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpListado;
+        private System.Windows.Forms.RadioButton rdbTodo;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.RadioButton rdbPlan;
+        private System.Windows.Forms.RadioButton rdbNombre;
+        private System.Windows.Forms.RadioButton rdbCodigo;
+        private System.Windows.Forms.ComboBox cmbPlan;
+    }
+}
